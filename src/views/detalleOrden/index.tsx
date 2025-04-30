7a5529f368eb3283e8759fbbf2921cc8c9dfd98a
AbelSalvadorTorres <mario.salvador@gruposid.com.mx>
2025-04-30 12:16:52 -0600

    [fix]: fix views in correct order

--- src/views/detalleOrden/index.tsx
+++ src/views/detalleOrden/index.tsx
@@ -1,62 +1,34 @@
 // material-ui
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import {
     Divider,
     Grid,
-    Stack,
     Table,
     TableBody,
     TableCell,
     TableContainer,
     TableHead,
     TableRow,
-    Typography,
-    TextField,
-    Switch,
-    Button
+    Typography
 } from '@mui/material';
 
 // project imports
-
-import axios from '../../utils/axios';
-import { gridSpacing } from '../../store/constant';
-import Chip from '../../ui-components/extended/Chip';
 import SubCard from '../../ui-components/cards/SubCard';
+import { gridSpacing } from '../../store/constant';
 import MainCard from '../../ui-components/cards/MainCard';
-import { ShipmentDetails, ShipmentValidate } from '../../types/shipment';
-import { useFormik } from "formik";
+import axios from '../../utils/axios';
+import { ShipmentDetails } from '../../types/shipment';
 
 const sxDivider = {
     borderColor: 'text.secondary'
 };
 
 
-const detalleOrden = () => {
+const detallePedido = () => {
+    const { idShipment = null } = useParams();
+    const [shipmentRows, setShipmentRows] = useState<ShipmentDetails[]>([])
 
-    const { idShipment } = useParams();
-    const [shipmentRows, setShipmentRows] = useState<ShipmentDetails[]>([]);
-    const [receivedData, setReceivedData] = useState<ShipmentValidate[]>([]);
-    const [initialValues, setInitialValues] = useState<{ receivedQuantities: Record<string, number> }>({ receivedQuantities: {} });
-
-    useEffect(()=>{
-        if(receivedData.length > 0){
-            console.log({receivedData})
-            const validateShipment = async () => {
-                try {
-                    const payload = {
-                        "shipment_details": receivedData
-                    }
-                    const response = await axios.post(`/v1/shipments/${idShipment}/validate`, payload);
-                    console.log({response})
-                } catch (error) {
-                    console.error("Error en la solicitud:", error);
-                }
-            }
-            validateShipment();
-        }
-    }
-    ,[receivedData]);
 
     useEffect(() => {
         if ( idShipment ) {
@@ -66,92 +38,21 @@ const detalleOrden = () => {
                     const { "shipment-details": shipmentDetails } = response.data.data
                     console.log({shipmentDetails})
                     setShipmentRows(shipmentDetails)
-                    setInitialValues({
-                        receivedQuantities: shipmentDetails.reduce((acc: ShipmentValidate, row: ShipmentDetails ) => ({ ...acc, [row._id]: row.quantity || 0 }), {})
-                    });
                 } catch (error) {
                     console.error("Error en la solicitud:", error);
                 }
-            }
+            };
             fetchData();
-        };
-    }, [idShipment]);
-
-    const formik = useFormik({
-        initialValues,
-        enableReinitialize: true,  // Permite reinicializar valores cuando cambian
-        onSubmit: values => {
-            const formattedData = shipmentRows.map(row => ({
-                shipment_detail_id: row._id,
-                accepted_quantity: values.receivedQuantities[row._id]
-            }));
-            console.log({ formattedData });
-            setReceivedData(formattedData);
         }
-    });
-
+    }, [idShipment]);
 
     return (
         <MainCard>
             <Grid container spacing={gridSpacing}>
-                <Grid size={{ xs: 12 }} >
-                    <SubCard title="Datos del pedido" >
-                        <Grid size={{ xs: 12 }}>
-                            <Divider sx={sxDivider} />
-                        </Grid>
+            <Grid size={{ xs: 12 }} >
+                    <SubCard title={`Datos del pedido:`} >
                         <Grid container spacing={gridSpacing} sx={{ p: 2.5 }}>
-                            <Grid size={{ xs: 12 }}>
-                                <Grid container spacing={gridSpacing}>
-                                    <Grid size={{ xs: 12, sm:6, md:4}} >
-                                        <Stack spacing={2}>
-                                            <Typography variant="h4">Identificador:</Typography>
-                                            <Stack spacing={0}>
-                                                <Stack direction="row" spacing={1}>
-                                                    <Typography variant="subtitle1">{shipmentRows[0]?.shipment.id}</Typography>
-                                                </Stack>
-                                            </Stack>
-                                        </Stack>
-                                    </Grid>
-                                    <Grid size={{ xs: 12, sm:6, md:4 }}>
-                                        <Stack spacing={2}>
-                                            <Typography variant="h4">Id embarque:</Typography>
-                                            <Stack spacing={0}>
-                                                <Stack direction="row" spacing={1}>
-                                                    <Typography variant="body2">{shipmentRows[0]?.shipment.load_id}</Typography>
-                                                </Stack>
-                                            </Stack>
-                                        </Stack>
-                                    </Grid>
-                                    <Grid size={{ xs: 12, sm:6, md:4 }}>
-                                        <Stack spacing={2} >
-                                            <Typography variant="h4">Estatus:</Typography>
-                                            <Stack direction="row" spacing={1}>
-                                                <Chip label="Aceptado" variant="outlined" size="small" chipcolor="success" />
-                                            </Stack>
-                                        </Stack>
-                                    </Grid>
-                                    <Grid size={{ xs: 12, sm:6, md:4 }}>
-                                        <Stack spacing={2}>
-                                            <Typography variant="h4">No. de orden:</Typography>
-                                            <Stack spacing={0}>
-                                                <Stack direction="row" spacing={1}>
-                                                    <Typography variant="body2">{shipmentRows[0]?.shipment.order_number}</Typography>
-                                                </Stack>
-                                            </Stack>
-                                        </Stack>
-                                    </Grid>
-                                    <Grid size={{ xs: 12, sm:6, md:4 }}>
-                                        <Stack spacing={2}>
-                                            <Typography variant="h4">Tipo de envío:</Typography>
-                                            <Stack spacing={0}>
-                                                <Stack direction="row" spacing={1}>
-                                                    <Typography variant="body2">{shipmentRows[0]?.shipment.shipment_type}</Typography>
-                                                </Stack>
-                                            </Stack>
-                                        </Stack>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
+                            <Typography variant="subtitle1">{idShipment}</Typography>
                             <Grid size={{ xs: 12 }}>
                                 <Divider sx={sxDivider} />
                             </Grid>
@@ -162,62 +63,42 @@ const detalleOrden = () => {
                     <SubCard content={false}>
                         <Grid container spacing={3}>
                             <Grid size={{ xs: 12 }}>
-                                <form onSubmit={formik.handleSubmit}>
-                                    <TableContainer>
-                                        <Table>
-                                            <TableHead>
-                                                <TableRow>
-                                                    <TableCell sx={{ pl: 3 }}>Identificador</TableCell>
-                                                    <TableCell align="left">Lote</TableCell>
-                                                    <TableCell align="left">Marca</TableCell>
-                                                    <TableCell align="center">Fecha de creación</TableCell>
-                                                    <TableCell align="center">Fecha de caducidad</TableCell>
-                                                    <TableCell align="center">Enviado</TableCell>
-                                                    <TableCell align="left">Recibido</TableCell>
-                                                    <TableCell align="left">Completo</TableCell>
+                                <TableContainer>
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell sx={{ pl: 3 }}>Identificador</TableCell>
+                                                <TableCell align="left">Lote</TableCell>
+                                                <TableCell align="left">Marca</TableCell>
+                                                <TableCell align="left">Id. embarque</TableCell>
+                                                <TableCell align="left">Tipo de envío</TableCell>
+                                                <TableCell align="left">No. de orden</TableCell>
+                                                <TableCell align="center">Fecha de creación</TableCell>
+                                                <TableCell align="center">Fecha de expiración</TableCell>
+                                                <TableCell align="center">Cantidad</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {shipmentRows?.map((row: ShipmentDetails, index: number) => (
+                                                <TableRow key={index}>
+                                                    <TableCell sx={{ pl: 3 }}>
+                                                        <Typography align="left" variant="subtitle1">
+                                                            {row.item.id}
+                                                        </Typography>
+                                                    </TableCell>
+                                                    <TableCell align="left">{row.lot}</TableCell>
+                                                    <TableCell align="left">{row.brand}</TableCell>
+                                                    <TableCell align="left">{row.shipment.load_id}</TableCell>
+                                                    <TableCell align="left">{row.shipment.shipment_type}</TableCell>
+                                                    <TableCell align="left">{row.shipment.order_number}</TableCell>
+                                                    <TableCell align="center">{new Date(row.created_at).toLocaleDateString()}</TableCell>
+                                                    <TableCell align="center">{ row.expiration_date ? new Date(row.expiration_date).toLocaleDateString() : null }</TableCell>
+                                                    <TableCell align="center">{row.quantity}</TableCell>
                                                 </TableRow>
-                                            </TableHead>
-                                            <TableBody>
-                                                {shipmentRows?.map((row: ShipmentDetails, index: number) => (
-                                                    <TableRow key={index}>
-                                                        <TableCell sx={{ pl: 3 }}>
-                                                            <Typography align="left" variant="subtitle1">
-                                                                {row.item.id}
-                                                            </Typography>
-                                                        </TableCell>
-                                                        <TableCell align="left">{row.lot}</TableCell>
-                                                        <TableCell align="left">{row.brand}</TableCell>
-                                                        <TableCell align="center">{new Date(row.created_at).toLocaleDateString()}</TableCell>
-                                                        <TableCell align="center">{ row.expiration_date ? new Date(row.expiration_date).toLocaleDateString() : null }</TableCell>
-                                                        <TableCell align="center">{row.quantity}</TableCell>
-                                                        <TableCell align="left">
-                                                            <TextField
-                                                               id={`recibido-${row._id}`}
-                                                               name={`receivedQuantities.${row._id}`}
-                                                               value={formik.values.receivedQuantities[row._id]}
-                                                               onChange={formik.handleChange}
-                                                               type="number"
-                                                               InputProps={{ inputProps: { min: 0, max: row.quantity } }}
-                                                            />
-                                                        </TableCell>
-                                                        <TableCell align="left">
-                                                        <Switch
-                                                           checked={formik.values.receivedQuantities[row._id] === row.quantity}
-                                                        />
-                                                        </TableCell>
-                                                    </TableRow>
-                                                ))}
-                                            </TableBody>
-                                        </Table>
-                                    </TableContainer>
-                                    <Grid  size={{ xs: 12 }} spacing={gridSpacing} sx={{ p: 2.5 }}>
-                                        <Stack direction="row" spacing={1} justifyContent="flex-end">
-                                            <Button variant="contained" type="submit">
-                                                    Guardar
-                                            </Button>
-                                        </Stack>
-                                    </Grid>
-                                </form>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
                             </Grid>
                         </Grid>
                     </SubCard>
@@ -227,4 +108,4 @@ const detalleOrden = () => {
     );
 };
 
-export default detalleOrden;
+export default detallePedido;
