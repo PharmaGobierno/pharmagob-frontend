2c6436599520f10a45bc2e70153c9fcd30ae82e9
Mario Hdez <mario.hernandez@gruposid.com.mx>
2025-04-30 08:03:10 -0600

    [feat]: Remove Auth component

---
+++ src/configs/axios.ts
@@ -0,0 +1,8 @@
+// Import into global components to have a previous config
+
+import axios from "axios";
+
+axios.defaults.baseURL = "https://pharma-gateway-682pqs65.uc.gateway.dev"
+axios.defaults.params = {
+    key: "AIzaSyBQFlqkqrc80WsmRmSJR4Lgm_YOGUvEYEg"
+}
