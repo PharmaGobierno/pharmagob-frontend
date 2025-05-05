a2d79f580adf16d76c4e39df905a38e54a7f55ba
AbelSalvadorTorres <mario.salvador@gruposid.com.mx>
2025-05-05 12:43:27 -0600

    [feat]: initial values

--- src/views/medicos/altaMedico/index.tsx
+++ src/views/medicos/altaMedico/index.tsx
@@ -6,12 +6,28 @@ import { gridSpacing } from '../../../store/constant';
 import SubCard from '../../../ui-components/cards/SubCard';
 import MainCard from '../../../ui-components/cards/MainCard';
 
+// constant
+const getInitialValues = () => {
+    const newEvent = {
+        umu_id: 'd2cf9ae4-52fe-4c1b-b6c1-d22f378fcc22',
+        name: '',
+        last_name_1: '',
+        employee_number: '',
+        profesional_licence: '',
+        specialty: '',
+        status: '',
+        service: []
+    };
+
+    return newEvent;
+};
+
 const AltaMedico = () => {    
     return (
         <MainCard>
             <Grid container spacing={gridSpacing}>
                 <Grid size={{ xs: 12 }} >
-                    <SubCard title="Alta de paciente " >
+                    <SubCard title="Alta de paciente" >
                         test
                     </SubCard>
                 </Grid>
