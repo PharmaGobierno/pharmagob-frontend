d7598f9d1672c5f1c124c3a0b9eb91f79b0d4a80
Hephaestus API <admin@cargamos.com>
2025-05-08 12:49:26 -0600

    adding pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     allowedHosts: [
         'node-port-pharmagob-frontend.default.svc.cluster.local'
-    ]
+    ],
     host: true,
     port: 8080,
     watch: {
